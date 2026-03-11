4dc2e6c35c0a12ff9e4efc8a187a3a41b26df56d
Roy Cesar's Asmad Torres <rcat@192.168.1.12>
2026-03-10 23:10:32 -0500

    Versión inicial WebsVoltio

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Importación principal de los Material Web Components
+// Esto activa las etiquetas <md-filled-button>, <md-icon>, etc.
+import '@material/web/all.js';
+
+// Aquí agregaremos la lógica futura (ej. animaciones, envío de formularios)
+console.log("WebsVoltio: Sistema iniciado correctamente.");
